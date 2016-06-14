46bbab170c0d5be945379aa182a0f64efb921c8b
Angel Boscan <angeldanielboscan@gmail.com>
2016-06-14 14:29:26 -0400

    Setting scripts, cleanup

--- node4-ruby-php5-android/entrypoint.sh
+++ node4-ruby-php5-android/entrypoint.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-
-cp -r /root/.android /home/teamcity/ >&2 /var/log/android.log
-chown -R teamcity:teamcity /home/teamcity >&2 /var/log/android.log
-
 /root/kvm-mknod.sh
-su -c "TEAMCITY_SERVER=$TEAMCITY_SERVER bash /setup-agent.sh run" teamcity
-
+su - teamcity -c "TEAMCITY_SERVER=$TEAMCITY_SERVER bash /start-agent.sh run"
