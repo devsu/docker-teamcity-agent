08c85da2dca24d36511e243c736286c3eacc8488
Angel Boscan <angeldanielboscan@gmail.com>
2016-06-14 16:30:55 -0400

    Changing commands to entrypoint

--- node4-ruby-php5-android/entrypoint.sh
+++ node4-ruby-php5-android/entrypoint.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+
+cp -rf /root/.android /home/teamcity/
+chown -R teamcity:teamcity /home/teamcity/
 bash /kvm-mknod.sh
 su - teamcity -c "TEAMCITY_SERVER=$TEAMCITY_SERVER bash /start-agent.sh run"
