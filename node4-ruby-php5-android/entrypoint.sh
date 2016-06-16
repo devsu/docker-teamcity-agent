0dd062104522541e403dde8ef47af7340589636c
Angel Boscan <angeldanielboscan@gmail.com>
2016-06-16 22:20:01 +0000

    Removing debug

--- node4-ruby-php5-android/entrypoint.sh
+++ node4-ruby-php5-android/entrypoint.sh
@@ -4,4 +4,4 @@ rsync -ruz /root/.android /home/teamcity/
 chown -R teamcity:teamcity /home/teamcity/
 chown -R teamcity:teamcity ${ANDROID_HOME}
 bash /kvm-mknod.sh
-su - teamcity -c "TEAMCITY_SERVER=$TEAMCITY_SERVER PATH=${PATH} bash /start-agent.sh run && $PATH"
+su - teamcity -c "TEAMCITY_SERVER=$TEAMCITY_SERVER PATH=${PATH} bash /start-agent.sh run"
