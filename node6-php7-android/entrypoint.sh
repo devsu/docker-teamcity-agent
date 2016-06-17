b274aef501fa5ec5c232057256a0e141c4852b1b
Cesar Salazar <csalazar@devsu.com>
2016-06-17 15:58:47 -0500

    make android executables added to PATH for teamcity user

--- node6-php7-android/entrypoint.sh
+++ node6-php7-android/entrypoint.sh
@@ -4,4 +4,4 @@ rsync -ruz /root/.android /home/teamcity/
 chown -R teamcity:teamcity /home/teamcity/
 chown -R teamcity:teamcity ${ANDROID_HOME}
 bash /kvm-mknod.sh
-su - teamcity -c "TEAMCITY_SERVER=$TEAMCITY_SERVER PATH=${PATH} bash /start-agent.sh run"
+sudo -Eu teamcity bash -c 'TEAMCITY_SERVER=$TEAMCITY_SERVER PATH=${PATH}:${ANDROID_HOME}/tools:${ANDROID_HOME}/platform-tools bash /start-agent.sh run'
