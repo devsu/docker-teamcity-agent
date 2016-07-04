4a1377b1b53a7acbd58c519a203b46d4c525c3ca
Cesar Salazar <csalazar@devsu.com>
2016-07-04 11:20:03 -0500

    fix an issue on node6-php7-android/entrypoint.sh that  was causing to loose the right HOME env variable

--- node6-php7-android/entrypoint.sh
+++ node6-php7-android/entrypoint.sh
@@ -4,4 +4,4 @@ rsync -ruz /root/.android /home/teamcity/
 chown -R teamcity:teamcity /home/teamcity/
 chown -R teamcity:teamcity ${ANDROID_HOME}
 bash /kvm-mknod.sh
-sudo -Eu teamcity bash -c 'TEAMCITY_SERVER=$TEAMCITY_SERVER PATH=${PATH}:${ANDROID_HOME}/tools:${ANDROID_HOME}/platform-tools bash /start-agent.sh run'
+su - teamcity -c "TEAMCITY_SERVER=$TEAMCITY_SERVER PATH=${PATH} bash /start-agent.sh run"
