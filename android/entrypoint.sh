41e7bf0ef347aeb9b940110161fdfaa693386c17
Cesar Salazar <csalazar@devsu.com>
2016-07-05 15:15:18 -0500

    fix entrypoint in Android variant

--- android/entrypoint.sh
+++ android/entrypoint.sh
@@ -3,5 +3,5 @@
 rsync -ruz /root/.android /home/teamcity/
 chown -R teamcity:teamcity /home/teamcity/
 chown -R teamcity:teamcity ${ANDROID_HOME}
-bash ${SCRIPTS}/kvm-mknod.sh
+bash /opt/scripts/kvm-mknod.sh
 su - teamcity -c "TEAMCITY_SERVER=$TEAMCITY_SERVER PATH=${PATH} bash /start-agent.sh run"
