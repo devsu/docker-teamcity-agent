9d33cd14e4e3378dcf5995c6123e518b0faa63ee
Cesar Salazar <csalazar@devsu.com>
2016-07-06 12:16:30 -0500

    fixing kvm configuration, and changing ownership of /dev/kvm to allow teamcity to start android emulator

--- android/entrypoint.sh
+++ android/entrypoint.sh
@@ -4,5 +4,5 @@ rsync -ruz /root/.android /home/teamcity/
 chown -R teamcity:teamcity /home/teamcity/
 chown -R teamcity:teamcity ${ANDROID_HOME}
 bash ${SCRIPTS}/kvm-mknod.sh
-bash ${SCRIPTS}/start-emulator.sh
+chown teamcity /dev/kvm
 su - teamcity -c "TEAMCITY_SERVER=$TEAMCITY_SERVER PATH=${PATH} bash /start-agent.sh run"
--- android/kvm-mknod.sh
+++ android/kvm-mknod.sh
@@ -3,22 +3,24 @@
 
 set -e
 
+echo >&2 "Starting kvm-mknod.sh script..."
+
 lsmod | grep '\<kvm\>' > /dev/null || {
   echo >&2 "KVM module not loaded; software emulation will be used"
   exit 1
 }
 
-mknod /dev/kvm c 10 $(grep '\<kvm\>' /proc/misc | cut -f 1 -d' ') || {
-  echo >&2 "Unable to make /dev/kvm node; software emulation will be used"
-  echo >&2 "(This can happen if the container is run without -privileged)"
-  exit 1
-}
+if [ ! -c /dev/kvm ]; then
+    mknod /dev/kvm c 10 $(grep '\<kvm\>' /proc/misc | cut -f 1 -d' ') || {
+      echo >&2 "Unable to make /dev/kvm node; software emulation will be used"
+      echo >&2 "(This can happen if the container is run without -privileged)"
+      exit 1
+    }
+fi
 
 dd if=/dev/kvm count=0 2>/dev/null || {
   echo >&2 "Unable to open /dev/kvm; qemu will use software emulation"
   echo >&2 "(This can happen if the container is run without -privileged)"
 }
 
-# authorize the kvm group to use KVM
-chown root:kvm /dev/kvm
-chmod g+w /dev/kvm
+echo >&2 "Script finished execution. If no errors reported, the /dev/kvm device node should be now created."
